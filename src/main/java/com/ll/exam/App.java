5f56434e716428c9c97a2b597c4def5c9176d209
nunusmohum <halojw301@gmail.com>
2022-07-07 15:05:57 +0900

    등록 기능 구현

--- src/main/java/com/ll/exam/App.java
+++ src/main/java/com/ll/exam/App.java
@@ -1,8 +1,10 @@
 package com.ll.exam;
 
+import java.util.HashMap;
 import java.util.Scanner;
 
 public class App {
+    private HashMap<String, String> phrases = new HashMap<>();
     public void run(){
         System.out.println("=== 명언 SSG ===");
 
@@ -16,10 +18,21 @@ public class App {
             switch(cmd){
                 case "종료":
                     break outer;
+                case "등록":
+                    System.out.printf("명언 : ");
+                    String phrase = sc.nextLine();
+                    System.out.printf("작가 : ");
+                    String writer = sc.nextLine();
+                    register(phrase, writer);
+                    System.out.printf("%d번 명언이 등록되었습니다.\n", phrases.size());
             }
         }
 
         sc.close();
 
+    }
+
+    private void register(String phrase, String writer){
+        phrases.put(phrase, writer);
     }
 }
